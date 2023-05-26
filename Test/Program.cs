d351a725c3d0fa9bd7e82585679ba6ffcf2aaafb
hungquang1205 <hungquang1205@gmail.com>
2023-05-26 21:33:43 +0700

    pc

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,7 +8,7 @@ namespace Test
 {
     internal class Program
     {
-        public const int BATCH_SIZE = 8;
+        public const int BATCH_SIZE = 1;
 
         static Random rand = new Random();
 
@@ -16,87 +16,92 @@ namespace Test
 
         static void Main(string[] args)
         {
-            DenseNeuralNetwork network = new DenseNeuralNetwork(
-                //new ActivationLayer(3, ActivationFunc.Tanh),
-                //new BatchNormLayer(3, ForwardLayer.ForwardPort.In),
-                6,
-                new BatchNormLayer(6, ForwardLayer.ForwardPort.In), 
-                new BatchNormLayer(4, ForwardLayer.ForwardPort.Out),
-                new ActivationLayer(4, ActivationFunc.Tanh),
-                new BatchNormLayer(4, ForwardLayer.ForwardPort.Out),
-                new ActivationLayer(4, ActivationFunc.Tanh),
-                new BatchNormLayer(3, ForwardLayer.ForwardPort.Out),
-                new ActivationLayer(3, ActivationFunc.Tanh),
-                1
-                );
+            //DenseNeuralNetwork network = new DenseNeuralNetwork(
+            //    //new ActivationLayer(3, ActivationFunc.Tanh),
+            //    //new BatchNormLayer(3, ForwardLayer.ForwardPort.In),
+            //    2,
+            //    new BatchNormLayer(2, ForwardLayer.ForwardPort.In),
+            //    new ActivationLayer(3, ActivationFunc.Linear),
+            //    new BatchNormLayer(3, ForwardLayer.ForwardPort.In),
+            //    new ActivationLayer(3, ActivationFunc.Linear),
+            //    new BatchNormLayer(3, ForwardLayer.ForwardPort.In),
+            //    1
+            //    );
+
+            RecurrentNeuralNetwork network = new RecurrentNeuralNetwork(3, 1, 1);
 
             string[] cats = UData.GetCategoriesFromCSV(@"C:\Users\mAy tInH 2 mAn HiNh\Documents\datasets\realtor-data.csv");
-            Dictionary<string, Tuple<double, double>> inputRanges, outputRanges;
+            Dictionary<string, AdditionNumericDataInfo> inputInfos, outputInfos;
             DistinctIntDataInfo[] distinctInfos;
-            UDataInfo info = new UDataInfo(
-                new string[]
-                {
-                    "status",
-                    "zip_code",
-                    "prev_sold_date",
-                    "price"
-                },
-                DataType.Neglect,
-                DataType.Double,
-                DataType.Double,
-                DataType.Double,
-                DataType.DistinctInt,
-                DataType.DistinctInt,
-                DataType.Neglect,
-                DataType.Double,
-                DataType.Neglect,
-                DataType.Neglect
-                );
-            Dictionary<string, double>[] inputs = UData.RetrieveUDataFromCSV(@"C:\Users\mAy tInH 2 mAn HiNh\Documents\datasets\realtor-data.csv", info, out distinctInfos, out inputRanges, 50000);
-            Dictionary<string, double>[] desiredOutputs = UData.RetrieveNumberDataFromCSV(@"C:\Users\mAy tInH 2 mAn HiNh\Documents\datasets\realtor-data.csv", 50000, out outputRanges, "price");
+            //UDataInfo info = new UDataInfo(
+            //    new string[]
+            //    {
+            //        "status",
+            //        "zip_code",
+            //        "prev_sold_date",
+            //        "price"
+            //    },
+            //    DataType.Neglect,
+            //    DataType.Double,
+            //    DataType.Double,
+            //    DataType.Double,
+            //    DataType.DistinctInt,
+            //    DataType.DistinctInt,
+            //    DataType.Neglect,
+            //    DataType.Double,
+            //    DataType.Neglect,
+            //    DataType.Neglect
+            //    );
+            //Dictionary<string, double>[] inputs = UData.RetrieveUDataFromCSV(@"C:\Users\mAy tInH 2 mAn HiNh\Documents\datasets\realtor-data.csv", info, out distinctInfos, out inputInfos, 100);
+            //Dictionary<string, double>[] inputs = UData.RetrieveNumberDataFromCSV(@"C:\Users\mAy tInH 2 mAn HiNh\Documents\datasets\realtor-data.csv", 5000, out inputInfos, "bed", "house_size");
+            //Dictionary<string, double>[] desiredOutputs = UData.RetrieveNumberDataFromCSV(@"C:\Users\mAy tInH 2 mAn HiNh\Documents\datasets\realtor-data.csv", 5000, out outputInfos, "price");
 
-            dataLength = inputs.Length;
+            //dataLength = inputs.Length;
 
-            DenseNNForwardResult result;
+            ForwardResult result;
 
-            //double[][] inputs =
-            //{
-            //    new double[] { 1, 0.5, 0.6 },
-            //    new double[] { 0.8, 0.4, 0.4 },
-            //    new double[] { 0.8, 0.4, 0.6 },
-            //    new double[] { 1, 0.4, 0.4 },
-            //    new double[] { 0.8, 0.5, 0.6 }
-            //};
+            double[][][] inputs =
+            {
+                new double[][] { new double[] { 0.25 }, new double[] { 0.75 }, new double[] { 0.5 } },
+                new double[][] { new double[] { 0.75 }, new double[] { 0.5 }, new double[] { 1 } },
+                new double[][] { new double[] { 0.5 }, new double[] { 1 }, new double[] { 0.25 } },
+                new double[][] { new double[] { 1 }, new double[] { 0.25 }, new double[] { 0.75 } }
+            };
 
-            //double[][] desiredOutputs =
-            //{
-            //    new double[] { 0.2, -0.9, 0.3, 0 },
-            //    new double[] { 0.1, -0.5, 0.7, 0 },
-            //    new double[] { 0.1, -0.5, 0.3, 0 },
-            //    new double[] { 0.2, -0.5, 0.7, 0 },
-            //    new double[] { 0.1, -0.9, 0.3, 0 }
-            //};
+            double[][] desiredOutputs =
+            {
+                new double[] { 1 },
+                new double[] { 0.25 },
+                new double[] { 0.75 },
+                new double[] { 0.5 }
+            };
 
             network.BiasAssignForEach(RandomDouble);
             network.WeightAssignForEach(RandomDouble);
 
-            for (int epoch = 0; epoch < 30000; epoch++)
+            dataLength = inputs.Length;
+
+            for (int epoch = 0; epoch < 100000; epoch++)
             {
                 int[] sampleIndexes = SampleIndex(0, dataLength, BATCH_SIZE);
-                double[][] sampleOutputs = ToDoubleArrays(Sample(desiredOutputs, sampleIndexes), outputRanges);
-
+                //int[] sampleIndexes = SampleIndex(0, dataLength, BATCH_SIZE);
+                //double[][] sampleOutputs = ToDoubleArrays(Sample(desiredOutputs, sampleIndexes), outputInfos);
+                
                 result = network.Forward(Sample(inputs, sampleIndexes));
+
+                double[][] sampleOutputs = new double[sampleIndexes.Length][];
+                for (int i = 0; i < sampleIndexes.Length; i++)
+                    sampleOutputs[i] = desiredOutputs[sampleIndexes[i]];
                 network.GradientDescent(sampleOutputs, result, 0.05 / BATCH_SIZE);
 
-                if ((epoch + 1) % 2000 == 0 || epoch == 0)
+                if ((epoch + 1) % 1000 == 0 || epoch == 0)
                 {
                     Console.WriteLine(epoch + 1 + " run: ");
                     // Console.Write(LogBatchNorm(network));
-                    // Console.WriteLine("Network: ");
                     // Console.Write(ToString(network));
-                    // Console.Write(NonForwardToString(network));
+                    // Console.WriteLine(NonForwardToString(network));
                     // LogOutput(result);
-                    LogCompareOutput(result, ToDoubleArrays(Sample(desiredOutputs, sampleIndexes)), outputRanges["price"]);
+                    LogCompareOutput(result, sampleOutputs);
 
                     double error = 0;
                     for (int i = 0; i < sampleOutputs.Length; i++)
@@ -106,27 +111,50 @@ namespace Test
                     Console.WriteLine();
                     Console.WriteLine("Error: " + error);
                     Console.WriteLine();
-                    if (error < 0.001 / (sampleOutputs.Length * sampleOutputs.Length))
-                        break;
+                    //if (error < 0.001 / (sampleOutputs.Length * sampleOutputs.Length))
+                    //    break;
                 }
-
-                //Console.WriteLine(epoch + 1 + " run: ");
-                //Console.WriteLine("Network: ");
-                //Console.Write(ToString(network));
-                //LogOutput(result);
-                //Console.WriteLine();
-
-                //double error = 0;
-                //for (int i = 0; i < sampleOutputs.GetLength(0); i++)
-                //    for (int j = 0; j < sampleOutputs.GetLength(1); j++)
-                //        error += Math.Pow(sampleOutputs[i, j] - result.outputs[i, j], 2);
-
-                //if (error < 0.001 / (sampleOutputs.Length * sampleOutputs.Length))
-                //    break;
-                //Console.WriteLine("Error: " + error);
             }
 
             Console.ReadKey();
+            while (true)
+            {
+                for (int epoch = 0; epoch < 1; epoch++)
+                {
+                    int[] sampleIndexes = SampleIndex(0, dataLength, BATCH_SIZE);
+                    //double[][] sampleOutputs = ToDoubleArrays(Sample(desiredOutputs, sampleIndexes), outputInfos);
+
+                    double[][] sampleOutputs = new double[sampleIndexes.Length][];
+                    for (int i = 0; i < sampleIndexes.Length; i++)
+                        sampleOutputs[i] = desiredOutputs[sampleIndexes[i]];
+
+                    result = network.Forward(Sample(inputs, sampleIndexes));
+                    network.GradientDescent(sampleOutputs, result, 0.05 / BATCH_SIZE);
+
+                    if ((epoch + 1) % 1 == 0 || epoch == 0)
+                    {
+                        Console.WriteLine(epoch + 1 + " run: ");
+                        //Console.WriteLine(LogBatchNorm(network));
+                        // Console.WriteLine(ToString(network));
+                        //Console.WriteLine(NonForwardToString(network));
+                        //Console.WriteLine(LogForwardLog(result, network.layers));
+                        // LogOutput(result);
+                        LogCompareOutput(result, sampleOutputs);
+
+                        double error = 0;
+                        for (int i = 0; i < sampleOutputs.Length; i++)
+                            for (int j = 0; j < sampleOutputs[0].Length; j++)
+                                error += Math.Pow(sampleOutputs[i][j] - result.outputs[i][j], 2);
+
+                        Console.WriteLine();
+                        Console.WriteLine("Error: " + error);
+                        Console.WriteLine();
+                        //if (error < 0.001 / (sampleOutputs.Length * sampleOutputs.Length))
+                        //    break;
+                    }
+                }
+                Console.ReadKey();
+            }
         }
 
         static int[] SampleIndex(int min, int max, int batchSize, int reservedPoint = int.MinValue)
@@ -161,29 +189,34 @@ namespace Test
             return result.ToArray();
         }
 
-        static double[][] Sample(double[][] population, params int[] indexes)
+        static ForwardInput[] Sample(double[][][] population, params int[] indexes)
         {
-            double[][] batch = new double[indexes.Length][];
+            ForwardInput[] batch = new ForwardInput[indexes.Length];
 
             for(int i = 0; i < indexes.Length; i++)
-                batch[i] = population[indexes[i]];
+                batch[i] = new ForwardInput(population[indexes[i]]);
 
             return batch;
         }
 
-        static double[][] Sample(double[][] population, int batchSize)
+        static ForwardInput[] Sample(double[][][] population, int batchSize)
         {
-            double[][] batch = new double[batchSize][];
+            return Sample(population, SampleIndex(0, population.Length, batchSize));
+        }
 
-            int currentIndex = 0;
-            for(int randCount = 0; randCount < batchSize; randCount++)
-            {
-                currentIndex += rand.Next(currentIndex, population.Length); // random false 
+        static ForwardInput[] Sample(double[][] population, params int[] indexes)
+        {
+            ForwardInput[] batch = new ForwardInput[indexes.Length];
 
-                batch[randCount] = population[currentIndex];
-            }
+            for(int i = 0; i < indexes.Length; i++)
+                batch[i] = new ForwardInput(population[indexes[i]]);
 
             return batch;
+        }
+
+        static ForwardInput[] Sample(double[][] population, int batchSize)
+        {
+            return Sample(population, SampleIndex(0, population.Length, batchSize));
         }
 
         static double[][] ToDoubleArrays(Dictionary<string, double>[] content)
@@ -205,7 +238,7 @@ namespace Test
             return result;
         }
 
-        static double[][] ToDoubleArrays(Dictionary<string, double>[] content, Dictionary<string, Tuple<double, double>> ranges)
+        static double[][] ToDoubleArrays(Dictionary<string, double>[] content, Dictionary<string, AdditionNumericDataInfo> infos)
         {
             List<string> keyList = new List<string>();
             foreach(string label in content[0].Keys)
@@ -219,10 +252,8 @@ namespace Test
                 result[sample] = new double[labels.Length];
                 for (int i = 0; i < labels.Length; i++)
                 {
-                    Tuple<double, double> range = ranges[labels[i]];
-
-                    if (range != null)
-                        result[sample][i] = (content[sample][labels[i]] - range.Item1) / (range.Item2 - range.Item1);
+                    if (infos.ContainsKey(labels[i]))
+                        result[sample][i] = infos[labels[i]].Normalize(NormalizationMode.DivideMean, content[sample][labels[i]]);
                     else
                         result[sample][i] = content[sample][labels[i]];
                 }
@@ -251,7 +282,7 @@ namespace Test
             return Sample(population, SampleIndex(0, dataLength, batchSize));
         }
 
-        static void LogOutput(DenseNNForwardResult result)
+        static void LogOutput(ForwardResult result)
         {
             double[][] outputs = result.outputs;
 
@@ -266,19 +297,12 @@ namespace Test
             }
         }
 
-        static void LogCompareOutput(DenseNNForwardResult result, double[][] desired, Tuple<double, double> range)
+        static void LogCompareOutput(ForwardResult result, double[][] desired)
         {
-            double[][] outputs = result.outputs;
-
-            for (int i = 0; i < outputs.Length; i++)
-                for (int j = 0; j < outputs[i].Length; j++)
-                    Console.WriteLine(NormRange(desired[i][j],range) + " : " + outputs[i][j], range);
-                    //Console.WriteLine(desired[i][j],range + " : " + DenormRange(outputs[i][j], range));
+            for (int i = 0; i < result.outputs.Length; i++)
+                for (int j = 0; j < result.outputs[i].Length; j++)
+                    Console.WriteLine(desired[i][j] + "\t:\t" + result.outputs[i][j]);
         }
-
-        static double DenormRange(double normed, Tuple<double, double> range) => normed * (range.Item2 - range.Item1) + range.Item1;
-
-        static double NormRange(double normed, Tuple<double, double> range) => (normed - range.Item1) / (range.Item2 - range.Item1);
 
         static string ToString(DenseNeuralNetwork network)
         {
@@ -368,10 +392,33 @@ namespace Test
             return sb.ToString();
         }
 
+        static string LogForwardLog(DenseNNForwardResult result, Layer[] layers)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int sample = 0; sample < result.outputs.Length; sample++)
+            {
+                for (int i = 0; i < result.layerInputs.Length; i++)
+                {
+                    if (layers[i] is ForwardLayer)
+                        continue;
+                    sb.AppendLine("Layer " + (i + 1) + ": ");
+                    for (int j = 0; j < result.layerInputs[i][sample].Length; j++)
+                        sb.Append(result.layerInputs[i][sample][j] + "\t");
+                    sb.AppendLine();
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
         static double RandomDouble()
         {
             //return rand.Next(1, 3);
-            return (rand.Next(0, 2) * 2 - 1) * (rand.NextDouble() * 0.3d + 0.1d);
+            //return (rand.Next(0, 2) * 2 - 1) * (rand.NextDouble() * 0.3d + 0.1d);
+            return (rand.NextDouble() * 0.3d + 0.1d);
         }
     }
 }
